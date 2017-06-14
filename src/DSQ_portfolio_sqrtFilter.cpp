2d76b665cd35a045c9cc7bf80a8173ad07c0ad0e
Piotrek Orłowski <piotrek.orlowski@gmail.com>
2017-06-14 09:16:44 +0000

    non-sqrt form filter updated to return fitted and predicted

--- src/DSQ_portfolio_sqrtFilter.cpp
+++ src/DSQ_portfolio_sqrtFilter.cpp
@@ -56,3 +56,28 @@ List portfolio_noStock_sqrtFilter(const arma::mat dataMat, const arma::vec initS
   
   return res;
 }
+
+//' @export
+//[[Rcpp::export]]
+List portfolio_noStock_simpleFilter(const arma::mat dataMat, const arma::vec initState, const arma::mat initProcCov, const List modelParams){
+  
+  // Set state handlers  
+  stateHandler transitionPtr = &affineTransitionStateHandler;
+  stateHandler observationPtr = &affineObservationStateHandler_optionPortfolios_noStock;
+  stateControl controlPtr = &affineAdditiveStateController;
+  
+  // Instantiate filter instance
+  ukfClass filterInstance(dataMat, initState, initProcCov, transitionPtr, observationPtr, controlPtr, modelParams);
+  
+  // Run filter
+  filterInstance.filterAdditiveNoise();
+  
+  // Set return variable
+  List res = List::create(Named("estimState") = filterInstance.getStateMat()
+                            , Named("stateCovCube") = filterInstance.getCovCube()
+                            , Named("logL") = filterInstance.getLogL()
+                            , Named("predMat") = filterInstance.getPredMat()
+                            , Named("fitMat") = filterInstance.getFitMat());
+  
+  return res;
+}
